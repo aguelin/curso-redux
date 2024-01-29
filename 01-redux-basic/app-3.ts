e63bdc990bc0a7e9b403e41164c94913c98bfec1
aguelin <alvaroguerrerolinares02@gmail.com>
2024-01-29 11:19:08 +0100

    feat: :sparkles: Bloque inicial completado

---
+++ 01-redux-basic/app-3.ts
@@ -0,0 +1,36 @@
+import { incrementadorAction, multiplicadorAction } from "./contador/contador.actions";
+import { contadorReducer } from "./contador/contador.reducer";
+import { Action, Reducer } from "./ngrx-fake/ngrx";
+
+class Store<T> {
+
+    // private state: T;
+
+    constructor( private reducer:Reducer<T>, private state:T){
+        // this.state = state;
+    }
+
+    getState(){
+        return this.state;
+    }
+
+    dispatch(action:Action){
+
+        this.state = this.reducer(this.state,action);
+
+    }
+
+}
+
+const store = new Store( contadorReducer, 10);
+
+console.log(store.getState());
+
+store.dispatch(incrementadorAction);
+
+console.log(store.getState());
+
+store.dispatch(multiplicadorAction);
+
+console.log(store.getState());
+
